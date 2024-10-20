b484ad6d1b49c66778ccb0c2e3dbf9de509fa180
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 14:50:08 +0700

    Replace createAction and createReducer with createSlice which does both

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,30 +1,28 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
+import { createSlice } from "@reduxjs/toolkit";
 
 // reducer
 let lastId = 0;
 
-export default createReducer([], {
-  // key: value
-  // actions: functions (event => event handler)
-  //  don't need to use ...state because under the hood, redux devtools uses immer -> so we can write mutating code
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-
-  [bugRemoved.type]: (bugs, action) => {
-    return bugs.filter((bug) => bug.id !== action.payload.id);
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+    bugRemoved: (bugs, action) => {
+      return bugs.filter((bug) => bug.id !== action.payload.id);
+    },
   },
 });
+
+export default slice.reducer;
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
