43ecd52e0a9485152897df87fc74285765de207f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 16:02:14 +0700

    Add new action, users reducer and selector for user1 bugs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import {
   bugAdded,
   bugRemoved,
   bugResolved,
+  bugAssignedToUser,
   getUnresolvedBugs,
+  getBugsByUser,
 } from "./store/bugs";
+import { userAdded } from "./store/users";
 import { projectAdded } from "./store/projects";
 import configureStore from "./store/configureStore";
 
@@ -13,12 +16,18 @@ store.subscribe(() => {
   console.log("store changed!");
 });
 
+store.dispatch(userAdded({ name: "User 1" }));
+store.dispatch(userAdded({ name: "User 2" }));
 store.dispatch(projectAdded({ name: "Project 1" }));
 store.dispatch(bugAdded({ description: "Bug 1" }));
 store.dispatch(bugAdded({ description: "Bug 2" }));
 store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 store.dispatch(bugResolved({ id: 1 }));
 store.dispatch(bugRemoved({ id: 2 }));
 
 const unresolvedBugs = getUnresolvedBugs(store.getState());
 console.log(unresolvedBugs);
+
+const user1Bugs = getBugsByUser(1)(store.getState());
+console.log(user1Bugs);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -8,6 +8,11 @@ const slice = createSlice({
   name: "bugs",
   initialState: [],
   reducers: {
+    bugAssignedToUser: (bugs, action) => {
+      const { bugId, userId } = action.payload;
+      const index = bugs.findIndex((bug) => bug.id === bugId);
+      bugs[index].userId = userId;
+    },
     bugAdded: (bugs, action) => {
       bugs.push({
         id: ++lastId,
@@ -26,17 +31,21 @@ const slice = createSlice({
 });
 
 export default slice.reducer;
-export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
-
-export const getUnresolvedBugs = (state) =>
-  state.entities.bugs.filter((bug) => !bug.resolved);
+export const { bugAdded, bugRemoved, bugResolved, bugAssignedToUser } =
+  slice.actions;
 
 // Create memoized selector from createselector
 // USing this selector if list of bug hasn't changed, the logic will not be executed again, it will return the result from the cache
 // So when called in many places will not re-execute -> e.g. will not have two different arrays in memory
 // By putting both bugs and project states, it checks both, if both hasn't changed it will not be re-computed
-createSelector(
+export const getUnresolvedBugs = createSelector(
   (state) => state.entities.bugs,
   (state) => state.entities.projects,
   (bugs, projects) => bugs.filter((bug) => !bug.resolved)
 );
+
+export const getBugsByUser = (userId) =>
+  createSelector(
+    (state) => state.entities.bugs,
+    (bugs) => bugs.filter((bug) => bug.userId === userId)
+  );
