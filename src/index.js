e9bd532d47aa14ca1b3b0c6d9b4f940d937d25a0
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 14:59:21 +0700

    Added a new store for projects and use that reducer and actions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
@@ -7,10 +7,12 @@ store.subscribe(() => {
   console.log("store changed!");
 });
 
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
-store.dispatch(actions.bugRemoved({ id: 2 }));
+// store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
+// store.dispatch(actions.bugRemoved({ id: 2 }));
+
+store.dispatch(actions.projectAdded({ name: "Project 1" }));
 
 console.log(store.getState());
