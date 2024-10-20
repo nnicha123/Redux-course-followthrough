8cbd573b64f6e081ea92535ebe1cf7a858f50460
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 15:31:06 +0700

    Added selector

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  getUnresolvedBugs,
+} from "./store/bugs";
 import { projectAdded } from "./store/projects";
 import configureStore from "./store/configureStore";
 
@@ -15,4 +20,5 @@ store.dispatch(bugAdded({ description: "Bug 3" }));
 store.dispatch(bugResolved({ id: 1 }));
 store.dispatch(bugRemoved({ id: 2 }));
 
-console.log(store.getState());
+const unresolvedBugs = getUnresolvedBugs(store.getState());
+console.log(unresolvedBugs);
