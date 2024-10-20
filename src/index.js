dbe940866107b2273e3216fd261438afca77a330
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 15:17:12 +0700

    Combining and structuring reducers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import * as actions from "./store/projects";
+import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
@@ -7,12 +8,11 @@ store.subscribe(() => {
   console.log("store changed!");
 });
 
-// store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-// store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-// store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-// store.dispatch(actions.bugResolved({ id: 1 }));
-// store.dispatch(actions.bugRemoved({ id: 2 }));
-
-store.dispatch(actions.projectAdded({ name: "Project 1" }));
+store.dispatch(projectAdded({ name: "Project 1" }));
+store.dispatch(bugAdded({ description: "Bug 1" }));
+store.dispatch(bugAdded({ description: "Bug 2" }));
+store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugRemoved({ id: 2 }));
 
 console.log(store.getState());
