3bd619f16d97000c473f994e0b49ee922d7c0f64
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-20 14:24:26 +0700

    configure store using reduxjs toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,6 @@
-import { createStore } from "redux";
 import reducer from "./bugs";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
