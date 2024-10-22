6ff31c1853ffc84f1a3682f07bac027f8ee70630
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-22 14:40:19 +0700

    Mocking HTTP calls -> unit test (so do not need to call backend and is faster)

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,13 +1,19 @@
 import { addBug } from "../bugs";
 import configureStore from "../configureStore";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
 describe("bugsSlice", () => {
   it("should handle the addBug action", async () => {
-    // dispatch(addBug) => store
-    const store = configureStore();
     const bug = { description: "b" };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
     console.log(store.getState().entities.bugs.list);
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
