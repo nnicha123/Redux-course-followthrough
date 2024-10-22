242c1d15a522cec4f504868f03c7387b7b2cc3e1
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-22 14:46:07 +0700

    Refactor to have clean tests and added error case

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -4,16 +4,32 @@ import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 
 describe("bugsSlice", () => {
-  it("should handle the addBug action", async () => {
+  let fakeAxios;
+  let store;
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureStore();
+  });
+
+  const bugsSlice = () => store.getState().entities.bugs;
+
+  it("should add the bug to the store if it's saved to the server", async () => {
     const bug = { description: "b" };
     const savedBug = { ...bug, id: 1 };
-
-    const fakeAxios = new MockAdapter(axios);
     fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-    const store = configureStore();
     await store.dispatch(addBug(bug));
-    console.log(store.getState().entities.bugs.list);
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    expect(bugsSlice().list).toContainEqual(savedBug);
+  });
+
+  it("should not add the bug to the store if it's not saved to the server", async () => {
+    const bug = { description: "b" };
+    fakeAxios.onPost("/bugs").reply(500);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugsSlice().list).toHaveLength(0);
   });
 });
