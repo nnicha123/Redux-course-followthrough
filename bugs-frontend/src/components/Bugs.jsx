1043dc73471036a5d0820e186ad3b1d17f64b606
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-23 10:28:38 +0700

    Connecting components and store using react-redux

--- bugs-frontend/src/components/Bugs.jsx
+++ bugs-frontend/src/components/Bugs.jsx
@@ -1,34 +1,17 @@
 import { Component } from "react";
-import StoreContext from "../contexts/storeContext";
 import { loadBugs } from "../store/bugs";
+import { connect } from "react-redux";
 
+// Subscribing and unsubscribing happens under the hood, do not need to sub/unsubscribe manually
 class Bugs extends Component {
-  static contextType = StoreContext;
-
-  state = { bugs: [] };
-
   componentDidMount() {
-    const store = this.context;
-
-    this.unsubscribe = store.subscribe(() => {
-      const bugsInStore = store.getState().entities.bugs.list;
-      if (this.state.bugs !== bugsInStore) {
-        this.setState({ bugs: bugsInStore });
-      }
-    });
-
-    store.dispatch(loadBugs());
-  }
-
-  componentWillUnmount() {
-    // Unsubscribe to make sure no memory leaks
-    this.unsubscribe();
+    this.props.loadBugs();
   }
 
   render() {
     return (
       <ul>
-        {this.state.bugs.map((bug) => (
+        {this.props.bugs.map((bug) => (
           <li key={bug.id}>{bug.description}</li>
         ))}
       </ul>
@@ -36,4 +19,15 @@ class Bugs extends Component {
   }
 }
 
-export default Bugs;
+// bugs: state.entities.bugs.list
+const mapStateToProps = (state) => ({
+  bugs: state.entities.bugs.list,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadBugs: () => dispatch(loadBugs()),
+});
+
+// Container
+// Presentation (Bugs)
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
